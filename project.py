37d9091b7c17e5c0bc1f8b0bb0a4e8b15887896f
Paolo Cosenza <113355625+paolocosenza@users.noreply.github.com>
2023-03-19 13:42:47 +0100

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as ho
+import plotly.graph_objects as go
 import h3
 import streamlit as st
 from catboost import CatBoostRegressor
