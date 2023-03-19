7f8a0e966fee4b9a5150ee77ee071e8d6d8dd146
Paolo Cosenza <113355625+paolocosenza@users.noreply.github.com>
2023-03-19 13:08:53 +0100

    Update project.py

--- project.py
+++ project.py
@@ -4,6 +4,15 @@ import h3
 import streamlit as st
 from catboost import CatBoostRegressor
 
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+     
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
+
 def main():
   model = CatBoostRegressor()
   model.load_model("model")
@@ -31,13 +40,13 @@ def main():
     st.write('Final coordinates:', h3.cell_to_latlng(end_h3))
              
     input_ = [start_lat, start_lng, end_lat, end_lng, distance_in_meters, 1, valhalla_time]
-    st.write('Predicted time for Monday, 8 A.M.', model.predict(np.array(input_)))
+    st.write('Predicted time for Monday, 8 A.M.', convert(model.predict(np.array(input_))))
 
     input_ = [start_lat, start_lng, end_lat, end_lng, distance_in_meters, 2, valhalla_time]
-    st.write('Predicted time for Thursday, 11 P.M.', model.predict(np.array(input_)))
+    st.write('Predicted time for Thursday, 11 P.M.', convert(model.predict(np.array(input_))))
 
     input_ = [start_lat, start_lng, end_lat, end_lng, distance_in_meters, 3, valhalla_time]    
-    st.write('Predicted time for Sunday, 3 P.M.', model.predict(np.array(input_)))
+    st.write('Predicted time for Sunday, 3 P.M.', convert(model.predict(np.array(input_))))
 
 if __name__ == "__main__":
     main()
