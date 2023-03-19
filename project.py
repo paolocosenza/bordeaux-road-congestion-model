9e6256e5f3c376a42f6b40de84695405c5afcc77
Paolo Cosenza <113355625+paolocosenza@users.noreply.github.com>
2023-03-19 12:04:42 +0100

    Add files via upload

---
+++ project.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import h3
+import streamlit as st
+from catboost import CatBoostClassifier
+
+model = CatBoostClassifier()
+model.load_model("model")
+
+dist_model = CatBoostClassifier()
+dist_model.load_model('dist_model')
+
+start_h3 = st.text_input("Type here", key="start_h3")
+
+end_h3 = st.text_input("Type here", key="end_h3")
+
+valhalla_time = st.text_input("Type here", key="valhalla_time")
+
+distance_in_meters = st.text_input("Type here", key="distance_in_meters")
+
+start_lat = h3.cell_to_latlng(start_h3)[0]
+start_lng = h3.cell_to_latlng(start_h3)[1]
+end_lat = h3.cell_to_latlng(end_h3)[0]
+end_lng = h3.cell_to_latlng(end_h3)[1]
+
+input_ = [start_lat, start_lng, end_lat, end_lng, distance_in_meters, 0, valhalla_time]
+model.predict(np.array(input_))
+st.write('Predicted time for Monday, 8 A.M.')
+
+input_ = [start_lat, start_lng, end_lat, end_lng, distance_in_meters, 1, valhalla_time]
+model.predict(np.array(input_))
+st.write('Predicted time for Thursday, 11 P.M.')
+
+input_ = [start_lat, start_lng, end_lat, end_lng, distance_in_meters, 2, valhalla_time]
+model.predict(np.array(input_))
+st.write('Predicted time for Sunday, 3 P.M.')
